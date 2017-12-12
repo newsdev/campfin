a9efc4d5840e32a80d0fec6f55c8958c2a3f46ab
Lilia Chang <liliachang@liliachang-wash.ent.nytint.com>
2017-12-12 11:53:57 -0500

    Got all the totals from candidates and rutgers data

--- campfin/get_candidates.py
+++ campfin/get_candidates.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import os
 import time
@@ -10,7 +11,8 @@ from utils import request_until_succeed, unicode_decode
 
 base = "https://api.open.fec.gov/v1/"
 house = "H"
-api_key = os.environ.get('FEC_KEY_2', None)
+api_key = os.environ.get('FEC_KEY_3', None)
+set_fields = set()
 
 """Returns list of candidates in given year"""
 def get_candidates(year):
@@ -22,7 +24,7 @@ def get_candidates(year):
     payload = []
     print("Fetching House candidates in year %s" %str(year))
     while has_next_page:
-        url = base + "/candidates/?page={}&per_page=100&office=H&sort=name&".format(str(page)) \
+        url = base + "/candidates/?page={}&per_page=100&sort=name&".format(str(page)) \
                 + election_request + "&" + api_request
                 
         results = json.loads(request_until_succeed(url))["results"]
@@ -48,7 +50,7 @@ def count_districts(candidates):
                 districts[key] = [candidate["candidate_id"]]
 
     return districts
-
+"""Sorts a dictionary according to length of value"""
 def sort_list(districts):
     district_list = []
     sorted_keys = sorted(districts, key=lambda k: len(districts[k]), reverse=True)
@@ -57,60 +59,82 @@ def sort_list(districts):
         district_list.append({"district":key, "candidates":districts[key]})
     return district_list
 
+"""Writes the list of districts:candidate_ids to a json file"""
 def write_district_dict(year, candidates):
     district_dict = count_districts(candidates)
     sorted_list = sort_list(district_dict)
     with open(year+"_districts.json", "w") as writefile:
         writefile.write(json.dumps(sorted_list, indent=4))
 
+"""API request for the candidate/{candidate_id}/totals"""
 def request_candidate_totals(candidate_id):
+    global api_key
+    time.sleep(0.5)
     url = base + "candidate/" + candidate_id + "/totals/"
     url = url + "?per_page=20&sort=-cycle&page=1&designation=P&api_key=" + api_key
-    time.sleep(0.5)
     data = json.loads(request_until_succeed(url))["results"]
     if data != []:
         return data[0]
     else:
+        print("no totals")
         return None
 
+"""Updates the key between the 4 in the environment so that 
+requests aren't throttled"""
+def update_key(num):
+    global api_key
+    index = num % 4
+    api_key = os.environ.get('FEC_KEY_%s' %str(index), None)
+
+"""Gets the totals from the candidates"""
 def get_self_funds(candidates):
     print("-------------------------------------------------------------")
     print("Retrieving self contributions from candidates.")
-    self_fund_dict = {}
+    self_funders = []
     total_candidates = len(candidates)
     curr = 1
-
+    global set_fields
     for candidate in candidates:
+        update_key(curr)
+        set_fields |= set(candidate.keys())
         print("candidate {} out of {}".format(str(curr), str(total_candidates)))
         totals = request_candidate_totals(candidate["candidate_id"])
+        """Get totals"""
         if totals is not None:
             amount = totals["candidate_contribution"]
-            self_fund_dict[candidate["candidate_id"]] = amount
+            set_fields |= set(totals.keys())
+            candidate.update(totals)
+            print(amount)
+        
+        self_funders.append(candidate)
         curr += 1
 
-    return self_fund_dict
+    return self_funders
 
+"""Dumps list of districts/candidates to json"""
 def read_districts():
     districts = []
     with open("data/2018_districts.json", "r") as readfile:
         districts = json.dumps(readfile.read())
     return districts
 
+"""Opens the csv of candidates and outputs list"""
+def get_candidates_csv(path):
+    candidates = []
+    with open(path, "r") as csvfile:
+        candidates = [{k: v for k, v in row.items()} \
+                for row in csv.DictReader(csvfile, skipinitialspace=True)]
+    return candidates
+        
 if __name__ == '__main__':
     year = "2018"
-    #Retrieve all House candidates
-    candidates = get_candidates(year)
-    #districts = read_districts()
-
-    #Write to file state_district:candidates
-    #write_district_dict(year, candidates)
+    #Retrieve all candidates
+    candidates = get_candidates_csv("data/2018_candidates.csv")
 
     #Get self funds of candidates
-    self_funds = get_self_funds(candidates)
-
-    with open("data/%s_self_fund.json" %year, "w") as writefile:
-        writefile.write(json.dumps(self_funds, indent=4))
-
-        
-
+    candidates_funds = get_self_funds(candidates)
+    with open("data/2018_candidates_funds_all.csv", "w") as csvfile:
+        dict_writer = csv.DictWriter(csvfile, list(set_fields))
+        dict_writer.writeheader()
+        dict_writer.writerows(candidates_funds)
 
---
+++ campfin/rutgers.py
@@ -0,0 +1,83 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+try:
+    from urllib.request import urlopen, Request
+except ImportError:
+    from urllib2 import urlopen, Request
+from utils import request_until_succeed, unicode_decode
+
+state_style = "background-color: rgb(221, 221, 221);"
+url = "http://cawp.rutgers.edu/buzz-2018-potential-women-candidates-us-congress-and-statewide-elected-executive#list"
+
+def get_table(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    rows = soup.select("table.election-watch-table tbody tr")
+    return rows
+
+def get_name(strs):
+    last = strs[-2]
+    rest = " ".join(strs[:-2])
+    return last + ", " + rest
+
+def get_party(strs):
+    return(strs[-1].strip("()"))
+
+def check_office(str):
+    if "U.S. Rep" in str:
+        return "House"
+    elif "U.S. Sen" in str:
+        return "Senate"
+    else:
+        return None
+
+def read_rows(table):
+    candidates = []
+    curr_state = ""
+    for row in table[1:]:
+        cells = row.findAll("td")
+        
+        state = cells[0].text.strip()
+        name_party = cells[3].text.strip()
+        office = check_office(cells[1].text.strip())
+
+        """Update state if needed""" 
+        if state != "":
+            curr_state = state
+            continue
+        elif office is not None:
+
+            """Split candidate name/party column into separate strings"""
+            name_party_split = name_party.split(" ")
+            name = get_name(name_party_split)
+            party = get_party(name_party_split)
+
+            """Fill dictiononary for candidate and append to data list"""
+            row_dict = {} 
+            row_dict["state"] = curr_state
+            row_dict["office"] = office
+            row_dict["district"] = cells[2].text.strip()
+            row_dict["name"] = name
+            row_dict["party"] = party
+            row_dict["general_seat"] = cells[4].text.strip()
+            row_dict["filing_date"] = cells[5].text.strip()
+            row_dict["primary_date"] = cells[6].text.strip()
+            row_dict["election_date"] = cells[7].text.strip()
+            candidates.append(row_dict)
+    return candidates
+    
+if __name__ == '__main__':
+    print("Fetching html data from cawp.rutgers.edu")
+    rows = get_table(url)
+
+    print("Converting html table to list of dictionaries")
+    candidates = read_rows(rows)
+
+    print("Writing candidates to data/2018_rutgers.csv")
+    keys = candidates[0].keys()
+    with open("data/2018_rutgers.csv", "w") as csvfile:
+        dict_writer = csv.DictWriter(csvfile, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(candidates)
