9f206bb14412f2103f243ebda6a6aa0df244d2f9
Lilia Chang <liliachang@liliachang-wash.ent.nytint.com>
2017-12-12 13:58:15 -0500

    merged csvs

---
+++ campfin/compare_rutgers.py
@@ -0,0 +1,54 @@
+import csv
+import os
+
+from get_candidates import get_candidates_csv
+
+updated_female = []
+updated_all = []
+
+def compare(females, all):
+    global updated_female
+    global updated_all
+    num_cands = len(all)
+    num_female = len(females)
+
+    females = sorted(females, key=lambda x:x['name'])
+    all = sorted(all, key=lambda x:x['name'])
+   
+    c_last = 0
+    f_last = 0
+    for c in range(c_last, num_cands):
+        for f in range(f_last, num_female):
+            if all[c]["name"].lower() == females[f]["name"].lower():
+                c_last = c + 1
+                f_last = f + 1
+                all[c]["rutgers_sex"] = "F"
+                females[f]["candidate_id"] = all[c]["candidate_id"]
+                #females[f]["committee_id"] = all[c]["committee_id"]
+                break
+    updated_female = females
+    updated_all = all
+                
+def write_to_csv(list, headers, path):
+    with open(path, "w") as csvfile:
+        dict_writer = csv.DictWriter(csvfile, headers)
+        dict_writer.writeheader()
+        dict_writer.writerows(list)
+    print("Wrote to %s ." %path)
+
+if __name__ == '__main__':
+    females = get_candidates_csv("data/2018_rutgers.csv")
+    all = get_candidates_csv("data/2018_candidates_funds_all.csv")
+    compare(females, all)
+
+    all_headers = set(all[0].keys())
+    all_headers.add("rutgers_sex")
+
+    female_headers = set(females[0].keys())
+    female_headers.add("candidate_id")
+    #female_headers.add("committee_id")
+
+    write_to_csv(updated_all, all_headers, "data/fec_gender_flag.csv")
+    write_to_csv(updated_female, female_headers, "data/rutgers_fec_ids.csv")
+
+
